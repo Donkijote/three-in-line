116eacc84b7b76ccacb871fb695c6f2d4a251514
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-08-02 17:55:35 +0200

    feat: setup tests config

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App.tsx";
+
+describe("App Test", () => {
+  test("Render App", () => {
+    render(<App />);
+    expect(screen.getByTestId("app")).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, type UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: ["./vite.setup-tests.ts"],
+    include: ["./src/**/*.test.ts", "./src/**/*.test.tsx"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: ["./src/main.tsx", "./*.config.*", "./*.d.*"],
+    },
+  },
+} as UserConfig);
