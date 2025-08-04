49481fe52e5a0278a4c8a8526b60fed3ba608d15
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-08-04 15:04:17 +0200

    feat: add tailwind and style config

---
+++ tailwind.config.ts
@@ -0,0 +1,5 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{ts,tsx,mdx}"],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    port: 3000,
+  },
   test: {
     environment: "jsdom",
     globals: true,
