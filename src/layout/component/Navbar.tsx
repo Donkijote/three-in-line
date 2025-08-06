f6bb35a1a6ee0523784146e13d6e6a4cf68cb397
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-08-06 16:29:34 +0200

    feat: add nav button and sidebar style

--- src/layout/component/Navbar.tsx
+++ src/layout/component/Navbar.tsx
@@ -1,8 +1,20 @@
 export const Navbar = () => {
   return (
-    <div className={"flex flex-row h-16 px-16 gap-4 items-center "}>
-      <button className={"cursor-pointer"}>button</button>
-      <h1 className={"text-2xl font-bold"}>3 in Line</h1>
-    </div>
+    <nav
+      className={
+        "flex flex-row h-16 px-16 gap-4 items-center border-b border-gray-200 shadow-xs"
+      }
+    >
+      <button className={"cursor-pointer max-h-6"}>
+        <span className="material-symbols-rounded">menu</span>
+      </button>
+      <h1
+        className={
+          "text-2xl block font-sans antialiased font-semibold leading-snug tracking-normal text-gray-700"
+        }
+      >
+        3 in Line
+      </h1>
+    </nav>
   );
 };
--- src/layout/component/Sidebar.tsx
+++ src/layout/component/Sidebar.tsx
@@ -1,3 +1,35 @@
 export const Sidebar = () => {
-  return <div>Here the Sidebar!</div>;
+  return (
+    <div className="fixed left-0 flex h-[calc(100dvh)] w-full max-w-[20rem] flex-col rounded-xl bg-white bg-clip-border p-4 text-gray-700 shadow-xl shadow-gray-900/5 transition-transform -translate-x-0">
+      <div className="p-4 mb-2">
+        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal ">
+          Matches History
+        </h5>
+      </div>
+      <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal">
+        <div
+          role="button"
+          className="flex items-center w-full p-3 cursor-pointer leading-tight transition-all rounded-lg outline-none text-start hover:bg-gray-100 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+        >
+          <div className="grid mr-4 place-items-center">
+            <span className="material-symbols-outlined text-green-400">
+              check
+            </span>
+          </div>
+          Victory
+        </div>
+        <div
+          role="button"
+          className="flex items-center w-full p-3 cursor-pointer leading-tight transition-all rounded-lg outline-none text-start hover:bg-gray-100 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+        >
+          <div className="grid mr-4 place-items-center">
+            <span className="material-symbols-outlined text-red-500">
+              close
+            </span>
+          </div>
+          Defeat
+        </div>
+      </nav>
+    </div>
+  );
 };
