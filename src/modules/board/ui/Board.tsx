0ce478e1a15e1bce61f0fc1b1e10f627e7c7600a
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-08-14 23:46:41 +0200

    feat: starting to handle game state

--- src/modules/board/ui/Board.tsx
+++ src/modules/board/ui/Board.tsx
@@ -1,9 +1,67 @@
 import { clsx } from "clsx";
+import { useCallback, useMemo, useState } from "react";
 
-const arr = Array.from({ length: 9 }, (_, i) => i % 2);
+const winningCombos = [
+  [0, 1, 2], // top row
+  [3, 4, 5], // middle row
+  [6, 7, 8], // bottom row
+  [0, 3, 6], // left column
+  [1, 4, 7], // middle column
+  [2, 5, 8], // right column
+  [0, 4, 8], // diagonal
+  [2, 4, 6], // diagonal
+];
+
+const GameState = {
+  WIN: "win",
+  LOST: "lost",
+  TIED: "tied",
+  PROGRESS: "progress",
+};
 
 export const Board = () => {
-  const isPlayerTurn = true; // This would typically come from a state or prop
+  const [isPlayerTurn, setIsPlayerTurn] = useState(true);
+  const [plays, setPlays] = useState<Array<number | null>>([
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ]);
+
+  const gameState = useMemo(() => {
+    let winner = null;
+    for (const [a, b, c] of winningCombos) {
+      if (plays[a] !== null && plays[a] === plays[b] && plays[a] === plays[c]) {
+        winner = plays[a];
+        break;
+      }
+    }
+    if (winner !== null && winner) return GameState.WIN;
+    if (winner !== null && !winner) return GameState.LOST;
+    if (!plays.includes(null) && winner === null) return GameState.TIED;
+    return GameState.PROGRESS;
+  }, [plays]);
+
+  const handleAction: (position: number) => void = useCallback(
+    (position: number) => {
+      if (plays[position] !== null) return;
+
+      setPlays((prevState) => {
+        const newValues = [...prevState];
+        newValues[position] = isPlayerTurn ? 1 : 0;
+        return newValues;
+      });
+      setIsPlayerTurn((prevState) => !prevState);
+    },
+    [isPlayerTurn, plays, setIsPlayerTurn],
+  );
+
+  const isGameInProgress = gameState === GameState.PROGRESS;
 
   return (
     <div className="flex h-[calc(100dvh-8rem)] w-full justify-start flex-col gap-12">
@@ -29,29 +87,35 @@ export const Board = () => {
             "grid grid-flow-row grid-cols-3 h-[calc(100dvh-18rem)] max-h-[812px] max-w-[1200px] m-auto"
           }
         >
-          {arr.map((item, index) => (
+          {plays.map((item, index) => (
             <button
               key={`${index}-${item}`}
               className={clsx(
-                "flex bg-background border-t-0 border-r-0 justify-center border border-grid items-center cursor-pointer aspect-square transition-all duration-150 hover:bg-white/5",
+                "flex bg-background border-t-0 border-r-0 justify-center border border-grid items-center aspect-square transition-all duration-150 ",
                 {
                   "border-b-0": index >= 6,
                   "border-l-0": index % 3 === 0,
+                  "cursor-pointer hover:bg-white/5": isGameInProgress,
                 },
               )}
+              onClick={() => handleAction(index)}
+              disabled={!isGameInProgress}
             >
-              <span
-                className={clsx(
-                  "material-symbols-rounded !text-7xl md:max-xl:!text-8xl xl:max-2xl:!text-8xl 2xl:!text-9xl",
-                  {
-                    "text-primary drop-shadow-[0_0_6px_var(--glow-blue)]": item,
-                    "text-secondary drop-shadow-[0_0_6px_var(--glow-red)]":
-                      !item,
-                  },
-                )}
-              >
-                {item ? "close" : "radio_button_unchecked"}
-              </span>
+              {item !== null && (
+                <span
+                  className={clsx(
+                    "material-symbols-rounded !text-7xl md:max-xl:!text-8xl xl:max-2xl:!text-8xl 2xl:!text-9xl",
+                    {
+                      "text-primary drop-shadow-[0_0_6px_var(--glow-blue)]":
+                        item,
+                      "text-secondary drop-shadow-[0_0_6px_var(--glow-red)]":
+                        !item,
+                    },
+                  )}
+                >
+                  {item ? "close" : "radio_button_unchecked"}
+                </span>
+              )}
             </button>
           ))}
         </div>
