2c436589e9ef99fcddfeb01655484ec287775ce6
Manuel Gonçalves <manuelmjgc@gmail.com>
2025-08-16 18:30:48 +0200

    test: fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,13 @@
+import { beforeAll } from "vitest";
+
 import { render, screen } from "@testing-library/react";
+
 import App from "./App.tsx";
 
 describe("App Test", () => {
+  beforeAll(() => {
+    window.matchMedia = vi.fn().mockImplementation(() => ({ matches: false }));
+  });
   test("Render App", () => {
     render(<App />);
     expect(screen.getByTestId("app")).toBeInTheDocument();
