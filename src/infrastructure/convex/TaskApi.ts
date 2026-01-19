c02c44017641d807dfb2b432aa3d6a3e2b551961
Manuel Gonçalves <manuelmjgc@gmail.com>
2026-01-19 23:29:53 +0100

    feat: add a convex mutation example

--- src/infrastructure/convex/TaskApi.ts
+++ src/infrastructure/convex/TaskApi.ts
@@ -1,7 +1,11 @@
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
 
 export function useTasks() {
   return useQuery(api.tasks.get);
+}
+
+export function useToggleTaskCompletion() {
+  return useMutation(api.tasks.toggleTaskCompletion);
 }
