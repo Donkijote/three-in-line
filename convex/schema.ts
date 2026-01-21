a92f6bb1392ef10448bd0a2d50a66ed45dfdd85a
Manuel Gonçalves <manuelmjgc@gmail.com>
2026-01-21 12:58:29 +0100

    feat: update user schema and auth provider

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,6 +5,18 @@ import { authTables } from "@convex-dev/auth/server";
 
 export default defineSchema({
   ...authTables,
+  users: defineTable({
+    name: v.optional(v.string()),
+    image: v.optional(v.string()),
+    email: v.optional(v.string()),
+    emailVerificationTime: v.optional(v.number()),
+    phone: v.optional(v.string()),
+    phoneVerificationTime: v.optional(v.number()),
+    isAnonymous: v.optional(v.boolean()),
+    codeName: v.optional(v.string()),
+  })
+    .index("email", ["email"])
+    .index("by_codeName", ["codeName"]),
   tasks: defineTable({
     text: v.string(),
     isCompleted: v.boolean(),
