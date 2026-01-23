d6c7c9ecba057fea39b01c7eac14f740c40d6782
Manuel Gonçalves <manuelmjgc@gmail.com>
2026-01-23 20:33:25 +0100

    feat: update user schema types

--- convex/schemas/user.ts
+++ convex/schemas/user.ts
@@ -1,5 +1,14 @@
 import { defineTable } from "convex/server";
-import { v } from "convex/values";
+import { type Infer, v } from "convex/values";
+
+const avatar = v.object({
+  type: v.union(
+    v.literal("custom"),
+    v.literal("preset"),
+    v.literal("generated"),
+  ),
+  value: v.string(),
+});
 
 export const UserSchema = defineTable({
   name: v.optional(v.string()),
@@ -10,16 +19,9 @@ export const UserSchema = defineTable({
   phoneVerificationTime: v.optional(v.number()),
   isAnonymous: v.optional(v.boolean()),
   username: v.optional(v.string()),
-  avatar: v.optional(
-    v.object({
-      type: v.union(
-        v.literal("custom"),
-        v.literal("preset"),
-        v.literal("generated"),
-      ),
-      value: v.string(),
-    }),
-  ),
+  avatar: v.optional(avatar),
 })
   .index("email", ["email"])
   .index("by_username", ["username"]);
+
+export type Avatar = Infer<typeof avatar>;
